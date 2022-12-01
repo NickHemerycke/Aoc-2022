44be41493a8a1153a7235b465c65d4fe382c7ece
Nick Hemerycke <nick.hemerycke@student.kuleuven.be>
2022-12-01 11:57:09 +0100

    almost final commit

--- adventDay1.py
+++ adventDay1.py
@@ -1,5 +1,6 @@
-calorieList = [1000,2000,3000,0,4000,0,5000,6000,0,7000,8000,9000,0,10000]
+calorieList = [1000,2000,3000,0,4000,0,5000,6000,0,7000,8000,9000,0,10000,0]
 
+elf = "elf"
 
 def elfCalorieCounter(calorie):
 
@@ -20,9 +21,53 @@ def elfCalorieCounter(calorie):
 
             calorieAmount = 0
 
-            print(ElfList)
 
-elfCalorieCounter(calorieList)
+    return(ElfList)
+
+def findBigElfs(calorie ):
+
+    bigElfList = []
+
+    counter = 1
+
+    output = elfCalorieCounter(calorieList)
+
+    bigNumber = max(output)
+
+    for i in elfCalorieCounter(calorieList):
+
+        if i == bigNumber:
+
+            bigElfList.append(counter)
+
+            counter = counter + 1
+
+        else:
+
+            counter = counter + 1
+
+    return bigElfList
+
+numbers = elfCalorieCounter(calorieList)
+
+biggestNumber = max(numbers)
+
+biggestNumber= str(biggestNumber)
+
+index = findBigElfs(calorieList)
+
+index = str(index)
+
+print(biggestNumber)
+
+print(index)
+
+print("elf or elves number " + index + " with a total of " + biggestNumber + " calories.")
+
+        
+
+
+
 
 
 
